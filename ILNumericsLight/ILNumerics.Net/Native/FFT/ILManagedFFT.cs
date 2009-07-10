2c382a385bc21e4ca74498e1aa7db0acf5ad4f29
mgualtieri2 <mgualtieri2@44a95196-4a49-0410-8dd5-ba7e9ef19c9b>
2009-07-10 12:32:47 +0000

    ILNumericsLight: Forgot to add ILManagedFFT and ILManagedLapack stubs.
    git-svn-id: http://svn.code.sf.net/p/ilnumericsnet/code@37 44a95196-4a49-0410-8dd5-ba7e9ef19c9b

---
+++ ILNumericsLight/ILNumerics.Net/Native/FFT/ILManagedFFT.cs
@@ -0,0 +1,134 @@
+﻿#region LGPL License
+/*    
+    This file is part of ILNumerics.Net Core Module.
+
+    ILNumerics.Net Core Module is free software: you can redistribute it 
+    and/or modify it under the terms of the GNU Lesser General Public 
+    License as published by the Free Software Foundation, either version 3
+    of the License, or (at your option) any later version.
+
+    ILNumerics.Net Core Module is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with ILNumerics.Net Core Module.  
+    If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System;
+using ILNumerics; 
+
+namespace ILNumerics.Native
+{
+    /// <summary>
+    /// C# implementation of IILFFT
+    /// </summary>
+    public class ILManagedFFT : IILFFT 
+    {
+        /// <summary>
+        /// performs backward n-dimensional fft
+        /// </summary>
+        /// <param name="A">input array</param>
+        /// <param name="nDims">number of dimensions of fft</param>
+        /// <returns>result, same size as A</returns>
+        public ILArray<complex> FFTBackward(ILArray<complex> A, int nDims)
+        {
+            throw new NotImplementedException("ILManagedFFT.FFTBackward. Is mkl_custom.dll on the PATH?");
+        }
+        /// <summary>
+        /// performs backward 1-dimensional fft 
+        /// </summary>
+        /// <param name="A">input array</param>
+        /// <param name="alongDim">dimensions to perform fft along</param>
+        /// <returns>result, same size as A</returns>
+        public ILArray<complex> FFTBackward1D(ILArray<complex> A, int alongDim)
+        {
+            throw new NotImplementedException("ILManagedFFT.FFTBackward1D. Is mkl_custom.dll on the PATH?");
+        }
+        /// <summary>
+        /// performs backward n-dimensional fft on hermitian sequence
+        /// </summary>
+        /// <param name="A">input array</param>
+        /// <param name="nDims">number of dimensions of fft</param>
+        /// <returns>result, same size as A</returns>
+        /// <remarks>This function brings increased performance if the implementation supports it. 
+        /// If not, the method will be implemented by repeated calls of (inplace) 1D fft.</remarks>
+        public ILArray<double> FFTBackwSym(ILArray<complex> A, int nDims)
+        {
+            throw new NotImplementedException("ILManagedFFT.FFTBackwSym. Is mkl_custom.dll on the PATH?");
+        }
+        /// <summary>
+        /// performs backward 1-dimensional fft on hermitian sequence
+        /// </summary>
+        /// <param name="A">input array</param>
+        /// <param name="alongDim">dimension to perform fft along</param>
+        /// <returns>result, same size as A</returns>
+        /// <remarks>This function brings increased performance if the implementation supports it. 
+        /// If not, the method will be implemented by repeated calls of (inplace) 1D fft.</remarks>
+        public ILArray<double> FFTBackwSym1D(ILArray<complex> A, int alongDim)
+        {
+            throw new NotImplementedException("ILManagedFFT.FFTBackwSym1D. Is mkl_custom.dll on the PATH?");
+        }
+        /// <summary>
+        /// performs n-dimensional fft
+        /// </summary>
+        /// <param name="A">input array</param>
+        /// <param name="nDims">number of dimension of fft</param>
+        /// <returns>result, same size as A</returns>
+        public ILArray<complex> FFTForward(ILArray<double> A, int nDims)
+        {
+            throw new NotImplementedException("ILManagedFFT.FFTForward. Is mkl_custom.dll on the PATH?");
+        }
+        /// <summary>
+        /// performs n-dimensional fft
+        /// </summary>
+        /// <param name="A">input array</param>
+        /// <param name="nDims">number of dimension of fft</param>
+        /// <returns>result, same size as A</returns>
+        public ILArray<complex> FFTForward(ILArray<complex> A, int nDims)
+        {
+            throw new NotImplementedException("ILManagedFFT.FFTForward. Is mkl_custom.dll on the PATH?");
+        }
+        /// <summary>
+        /// performs 1-dimensional fft
+        /// </summary>
+        /// <param name="A">input array</param>
+        /// <param name="alongDim">dimension to perform fft along</param>
+        /// <returns>result, same size as A</returns>
+        public ILArray<complex> FFTForward1D(ILArray<complex> A, int alongDim)
+        {
+            throw new NotImplementedException("ILManagedFFT.FFTForward1D. Is mkl_custom.dll on the PATH?");
+        }
+        /// <summary>
+        /// performs 1-dimensional fft
+        /// </summary>
+        /// <param name="A">input array</param>
+        /// <param name="alongDim">dimension to perform fft along</param>
+        /// <returns>result, same size as A</returns>
+        public ILArray<complex> FFTForward1D(ILArray<double> A, int alongDim)
+        {
+            throw new NotImplementedException("ILManagedFFT.FFTForward1D. Is mkl_custom.dll on the PATH?");
+        }
+        /// <summary>
+        /// true, if the implementation caches plans between subsequent calls
+        /// </summary>
+        public bool CachePlans {
+            get { return false; }
+        }
+        /// <summary>
+        /// Clear all currently cached plans
+        /// </summary>
+        public void FreePlans() {}
+        /// <summary>
+        /// true, if the implementation efficiently transforms from/to hermitian sequences (hermitian symmetry). 
+        /// </summary>
+        /// <remarks>If this property returns 'true', the implementation brings increased performance. 
+        /// If not, the symmetry methods will bring no performance advantage over the 1D transforms. </remarks>
+        public bool SpeedyHermitian { 
+            get { return false; }
+        }
+    }
+}
