57f8ad0f7ce29e5e3cdfd264af491fa9bef7ed4e
joemoorhouse <joemoorhouse@44a95196-4a49-0410-8dd5-ba7e9ef19c9b>
2009-11-15 21:02:05 +0000

    Added implementation of 2D and 3D meshgrid functions.
    git-svn-id: http://svn.code.sf.net/p/ilnumericsnet/code@61 44a95196-4a49-0410-8dd5-ba7e9ef19c9b

--- ILNumerics/Functions/builtin/meshgrid.cs
+++ ILNumerics/Functions/builtin/meshgrid.cs
@@ -21,12 +21,133 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ILNumerics.Misc;
+using ILNumerics.Exceptions; 
 
 namespace ILNumerics.BuiltInFunctions {
 
-    public partial class ILMath {         
-        //public static void meshgrid (ILArray<double> x, ILArray<double> y, ILArray<double> z) {
-            
-        //}
+    public partial class ILMath
+    {
+        /// <summary>
+        /// Create two 2D arrays for the valuation and visualization of functions of two variables 
+        /// </summary>
+        /// <param name="x">Vector of x values</param>
+        /// <param name="y">Vector of y values</param>
+        /// <returns>A List of two arrays, the x vector values along the first dimension of the first array, and y vector values along the second dimension of the second array</returns>
+        /// <remarks></remarks>
+        public static List<ILArray<double>> meshgrid(ILArray<double> x, ILArray<double> y)
+        {
+            if (!x.IsVector || !y.IsVector)
+            {
+                throw new ILArgumentException("meshgrid: inputs must be vectors");
+            }
+            int xLength = x.Length;
+            int yLength = y.Length;
+            ILDimension dimRet = new ILDimension(xLength, yLength);
+            double[] retArrX = ILMemoryPool.Pool.New<double>(dimRet.NumberOfElements);
+            double[] retArrY = ILMemoryPool.Pool.New<double>(dimRet.NumberOfElements);
+            unsafe
+            {
+                fixed (double* pRetArrX = retArrX)
+                fixed (double* pRetArrY = retArrY)
+                fixed (double* pX = x.m_data)
+                fixed (double* pY = y.m_data)
+                {
+                    double* pRetArrayX = pRetArrX;
+                    double* pRetArrayY = pRetArrY;
+                    double* pArrayX;
+                    double* pArrayY = pY;
+                    double* pXEnd = pX + xLength;
+                    double* pYEnd = pY + yLength;
+                    while (pArrayY < pYEnd)
+                    {
+                        pArrayX = pX;
+                        while (pArrayX < pXEnd)
+                        {
+                            *pRetArrayX = *pArrayX;
+                            *pRetArrayY = *pArrayY;
+                            pArrayX++;
+                            pRetArrayX++;
+                            pRetArrayY++;
+                        }
+                        pArrayY++;
+                    }
+
+                }
+            }
+            List<ILArray<double>> retList = new List<ILArray<double>>();
+            retList.Add(new ILArray<double>(retArrX, dimRet));
+            retList.Add(new ILArray<double>(retArrY, dimRet));
+            return retList;
+        }
+
+        /// <summary>
+        /// Create three 3D arrays for the valuation and visualization of functions of three variables 
+        /// </summary>
+        /// <param name="x">Vector of x values</param>
+        /// <param name="y">Vector of y values</param>
+        /// <param name="z">Vector of z values</param>
+        /// <returns>A List of three arrays, the rows of the first comprising the x vector values and the columns of the second comprising the y vector values</returns>
+        /// <remarks></remarks>
+        public static List<ILArray<double>> meshgrid(ILArray<double> x, ILArray<double> y, ILArray<double> z)
+        {
+            if (!x.IsVector || !y.IsVector || !z.IsVector)
+            {
+                throw new ILArgumentException("meshgrid: inputs must be vectors");
+            }
+            int xLength = x.Length;
+            int yLength = y.Length;
+            int zLength = z.Length;
+            ILDimension dimRet = new ILDimension(xLength, yLength, zLength);
+            double[] retArrX = ILMemoryPool.Pool.New<double>(dimRet.NumberOfElements);
+            double[] retArrY = ILMemoryPool.Pool.New<double>(dimRet.NumberOfElements);
+            double[] retArrZ = ILMemoryPool.Pool.New<double>(dimRet.NumberOfElements);
+            unsafe
+            {
+                fixed (double* pRetArrX = retArrX)
+                fixed (double* pRetArrY = retArrY)
+                fixed (double* pRetArrZ = retArrZ)
+                fixed (double* pX = x.m_data)
+                fixed (double* pY = y.m_data)
+                fixed (double* pZ = z.m_data)
+                {
+                    double* pRetArrayX = pRetArrX;
+                    double* pRetArrayY = pRetArrY;
+                    double* pRetArrayZ = pRetArrZ;
+                    double* pArrayX;
+                    double* pArrayY;
+                    double* pArrayZ = pZ;
+                    double* pXEnd = pX + xLength;
+                    double* pYEnd = pY + yLength;
+                    double* pZEnd = pZ + zLength;
+                    while (pArrayZ < pZEnd)
+                    {
+                        pArrayY = pY;
+                        while (pArrayY < pYEnd)
+                        {
+                            pArrayX = pX;
+                            while (pArrayX < pXEnd)
+                            {
+                                *pRetArrayX = *pArrayX;
+                                *pRetArrayY = *pArrayY;
+                                *pRetArrayZ = *pArrayZ;
+                                pArrayX++;
+                                pRetArrayX++;
+                                pRetArrayY++;
+                                pRetArrayZ++;
+                            }
+                            pArrayY++;
+                        }
+                        pArrayZ++;
+                    }
+
+                }
+            }
+            List<ILArray<double>> retList = new List<ILArray<double>>();
+            retList.Add(new ILArray<double>(retArrX, dimRet));
+            retList.Add(new ILArray<double>(retArrY, dimRet));
+            retList.Add(new ILArray<double>(retArrZ, dimRet));
+            return retList;
+        }
     }
 }
